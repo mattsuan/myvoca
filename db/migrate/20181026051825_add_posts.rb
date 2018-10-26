b4df0773ad02572f6c5852a78d6365a5108082cf
tmatsuda@netprotections.co.jp <tmatsuda@netprotections.co.jp>
2018-10-26 14:35:42 +0900

    DBの修正

--- db/migrate/20181026051825_add_posts.rb
+++ db/migrate/20181026051825_add_posts.rb
@@ -5,7 +5,7 @@ class AddPosts < ActiveRecord::Migration
     add_column :posts, :wiki_data, :text
     add_column :posts, :category_name, :text
     add_column :posts, :category_color, :text
-    add_column :wiki_data, :post_content, :text
+    add_column :wikidata, :post_content, :text
     add_column :categories, :category_color, :text
   end
 end
